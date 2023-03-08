9d2b5e5db5ba6e4faf7b462befd584df23f4a43e
Adriano Silva <adrianowic0101@gmail.com>
2023-03-08 18:58:10 -0300

    Criado um ENUM de categoria com 3 propriedades e chamado no type Tproduto. Foi criado funções para: Adicionar um novo usuario ao array de usuarios, mostrar todos os usuarios do array de usuarios, adicionar um novo produto no array de produtos, mostrar os produtos do array de produtos, mostrar um produto especifico pelo Id, mostrar um produto especifico pelo nome, criar uma nova compra no array de compras e buscar compras feitas pelo o Id do usuario.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-import {user, products, purchase} from "./database"
+import {user, products, purchase, createUser, getAllUsers, createdProduct, getAllProduct, getProductById, queryProductsByName, createPurchase, getAllPurchasesFromUserId} from "./database"
+import { Category } from "./types"
 
-console.table(user)
-console.table(products)
-console.table(purchase)
+// console.table(user)
+// console.table(products)
+// console.table(purchase)
+
+// createUser("003", "ciente03@email.com", "125478")
+
+// console.table(getAllUsers())
+
+// createdProduct('p003', 'Teclado sem fio', 210, Category.ELETRONICS)
+
+// console.table(getAllProduct())
+
+ getProductById("p002")
+
+// queryProductsByName('MEMORIA RAM')
+
+// createPurchase("003", "p002", 4, 500 )
+
+// getAllPurchasesFromUserId("004")
