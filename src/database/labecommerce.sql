9e423bebac158fe9785066f3d39d20885a745958
Adriano Silva <adrianowic0101@gmail.com>
2023-04-11 08:39:03 -0300

    Criada a tabela 'purchase_products' e feita a relação m:n dela com as tabelas 'purchases' e 'products'.

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -26,6 +26,7 @@ VALUES ("p001", "Monitor", 95.80, "Eletrônicos"),
        ("p005", "Tenis AllStar", 110.80, "Roupas e calçados");
 
 
+-- implementação da branch relações-sql-l
 CREATE TABLE purchases(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     total_price REAL NOT NULL,
@@ -37,23 +38,53 @@ CREATE TABLE purchases(
 );
 
 INSERT INTO purchases(id, total_price, paid, delivered_at, buyer_id) 
-VALUES ("pe001", 191.6, 1, "", "001"),
-       ("pe002", 50, 1, "", "001"),
-       ("pe003", 71, 1, "", "002"),
-       ("pe004", 31.6, 1, "", "002"),
-       ("pe005", 221.6, 1, "", "003"),
-       ("pe006", 47.4, 1, "", "003");
+VALUES ("c001", 95.80, 1, "", "001"),
+       ("c002", 25, 1, "", "001"),
+       ("c003", 35.50, 1, "", "002"),
+       ("c004", 15.80, 1, "", "002"),
+       ("c005", 110.80, 1, "", "003"),
+       ("c006", 15.80, 1, "", "003");
 
 SELECT * FROM purchases;
 
 UPDATE purchases 
 SET delivered_at = DATETIME('now', 'localtime')
-WHERE id = "pe005";
+WHERE id = "c005";
 
 SELECT * FROM purchases
 INNER JOIN users
 ON purchases.buyer_id = users.id
 WHERE users.id = "003";
+
+
+-- implementação da branch relações-sql-ll
+CREATE TABLE purchases_products(
+    purchase_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+
+    FOREIGN KEY(purchase_id) REFERENCES purchases(id),
+    FOREIGN KEY(product_id) REFERENCES products(id)
+    );
+
+INSERT INTO purchases_products VALUES
+("c001", "p004", 2),
+("c003", "p001", 2),
+("c005", "p005", 1);
+
+SELECT 
+    purchases.buyer_id as usuarioId,
+    purchases.id as compraId,
+    products.id as produtoId,
+    products.name as produdoNome,
+    products.price as produtoPreco,
+    purchases_products.quantity 
+FROM purchases_products
+INNER JOIN purchases
+ON purchases_products.purchase_id = purchases.id
+INNER JOIN products
+ON purchases_products.product_id = products.id;
+
 
 --Get All Users
 SELECT * FROM users;
